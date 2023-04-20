ca0c4ed9380e8ebf86e64913dcfb1aef6d0163fc
jaznahmendez <jazmin.mendez@iteso.mx>
2023-04-20 12:20:57 -0600

    angular-theme

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -3,26 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './layouts/footer/footer.component';
-import { NavComponent } from './layouts/nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import {MatCommonModule} from '@angular/material/core';
 import { AdminComponent } from './pages/admin/admin.component';
 import { PatientComponent } from './pages/patient/patient.component';
 import { ProfessionalComponent } from './pages/professional/professional.component';
 import { TutorComponent } from './pages/tutor/tutor.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { NavComponent } from './layouts/nav/nav.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
-    NavComponent,
     AdminComponent,
     PatientComponent,
     ProfessionalComponent,
-    TutorComponent
+    TutorComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCommonModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
