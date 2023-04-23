c56abb7d3b94c754af8a3d394e0733661101f350
jaznahmendez <jazmin.mendez@iteso.mx>
2023-04-23 17:50:06 -0600

    profile bug

--- Front-end/src/app/pages/professional/profile/profile.component.ts
+++ Front-end/src/app/pages/professional/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Patient } from 'src/app/shared/interfaces/patient';
 import { Professional } from 'src/app/shared/interfaces/professional'
+import { PatientService } from 'src/app/shared/services/patient.service';
 import { ProfessionalService } from 'src/app/shared/services/professional.service'
 
 @Component({
@@ -12,12 +14,37 @@ export class ProfileComponent implements OnInit {
   }
   idProf: String = ''
 
-  constructor(private professionalService: ProfessionalService) {}
+  patients: any = []
+  pArray: any = []
+  patientsProf: any = []
+  imageLinkCp : any =  [];
+
+  constructor(private professionalService: ProfessionalService, private patientService: PatientService) {}
 
   ngOnInit(): void {
     this.professionalService.setProfessionalProfile('641e3aa760a550973418d30e')
     this.professionalService.getProfessional().subscribe((response: any) => {
       this.professional = response
+    });
+
+    this.patientService.getPatients().subscribe((response: any) => {
+      this.patients= response.patient;
+      
+      
+      for(let i = 0; i < this.patients.length; i++)
+      {
+        for(let j = 0; j < this.patients[i].currentProffesionals.length; j++)
+        {
+          if(this.patients[i].currentProffesionals[j] == this.professional._id)
+          {
+            this.patientsProf.push(this.patients[i])
+            this.imageLinkCp.push("url('https://randomuser.me/api/portraits/women/" + i + ".jpg')");
+          }
+        }
+      }
+
+      console.log(this.patientsProf)
+      
     });
   }
 
