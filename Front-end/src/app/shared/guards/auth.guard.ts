e8948804acd9641feb809e390ceae1df0493ae5c
jaznahmendez <jazmin.mendez@iteso.mx>
2023-05-05 16:27:38 -0600

    log out btn

--- Front-end/src/app/shared/guards/auth.guard.ts
+++ Front-end/src/app/shared/guards/auth.guard.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { TokenService } from '../services/token.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(private tokenService: TokenService, private router: Router){}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      if(this.tokenService.isAuth()) {
+        return true;
+      } else {
+        this.router.navigate(['/']);
+        return false;
+      }
   }
   
 }
